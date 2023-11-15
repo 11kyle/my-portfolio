ff1dc0532a9edb02749175c5b6bf0ae13328464e
Kyle Johnson <11kyle.johnson@gmail.com>
2023-11-14 21:53:10 -0600

    added media

--- src/components/right-overlay.jsx
+++ src/components/right-overlay.jsx
@@ -2,7 +2,7 @@ export default function RightOverlay() {
   return (
     <div className="hidden md:block fixed right-4 lg:right-8 bottom-0">
       <div className="flex flex-col items-center space-y-6">
-          <a href="mailto:11kyle.johnson@gmail.com" className="text-slate-400 text-sm" style={{ writingMode: "vertical-lr"}}>11kyle.johnson@gmail.com</a>
+          <a href="mailto:11kyle.johnson@gmail.com" className="text-slate-400 text-sm hover:text-broncos-orange" style={{ writingMode: "vertical-lr"}}>11kyle.johnson@gmail.com</a>
           <div className="h-20 w-[1px] bg-slate-400"></div>
         </div>
     </div>
